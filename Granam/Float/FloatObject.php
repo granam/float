90005011d5c57415d206555096cab5271f8354ad
jaroslavtyc <mail@jaroslavtyc.com>
2016-04-25 20:38:47 +0200

    Thrown exceptions annotated

--- Granam/Float/FloatObject.php
+++ Granam/Float/FloatObject.php
@@ -11,6 +11,8 @@ class FloatObject extends NumberObject implements FloatInterface
      * @param mixed $value
      * @param bool $strict = true allows only explicit values, not null and empty string
      * @param bool $paranoid Throws exception if some value is lost on cast because of rounding
+     * @throws \Granam\Float\Tools\Exceptions\WrongParameterType
+     * @throws \Granam\Float\Tools\Exceptions\ValueLostOnCast
      */
     public function __construct($value, $strict = true, $paranoid = false)
     {
--- Granam/Float/Tools/ToFloat.php
+++ Granam/Float/Tools/ToFloat.php
@@ -10,6 +10,8 @@ class ToFloat
      * @param bool $strict = true allows only explicit values, not null and empty string
      * @param bool $paranoid Throws exception if some value is lost on cast due to rounding on cast
      * @return float
+     * @throws \Granam\Float\Tools\Exceptions\WrongParameterType
+     * @throws \Granam\Float\Tools\Exceptions\ValueLostOnCast
      */
     public static function toFloat($value, $strict = true, $paranoid = false)
     {
@@ -17,6 +19,14 @@ class ToFloat
         return (float)self::convertToNumber($value, $strict, $paranoid);
     }
 
+    /**
+     * @param $value
+     * @param bool $strict
+     * @param bool $paranoid
+     * @return float|int
+     * @throws \Granam\Float\Tools\Exceptions\WrongParameterType
+     * @throws \Granam\Float\Tools\Exceptions\ValueLostOnCast
+     */
     private static function convertToNumber($value, $strict, $paranoid)
     {
         try {
